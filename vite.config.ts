b99fa11a475793406aea0fa98072c77e4eb34c5f
Florentino Navarro <tino.navrro@hotmail.com>
2022-06-25 13:23:27 -0500

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Icons from 'unplugin-icons/vite'
+import viteImagemin from 'vite-plugin-imagemin'
+import { qrcode } from 'vite-plugin-qrcode';
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +14,36 @@ export default defineConfig({
   },
   plugins: [
     react(),
-  ]
+    Icons({ /* options */ }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+    qrcode(),
+    removeConsole(),
+  ],
+  // base: 'boilerplate-vite-react-typescript-sass-edgrid'
 })
